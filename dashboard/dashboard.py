e81ef9ecd0f47f8d7a140262109ad1eeb953824d
stefan-bergstein <stefan.bergstein@gmail.com>
2021-01-23 14:48:58 +0100

    dashboard

---
+++ dashboard/dashboard.py
@@ -0,0 +1,88 @@
+import logging
+from flask import Flask, request, make_response, render_template
+from flask_socketio import SocketIO
+import json
+
+
+app = Flask(__name__)
+sio = SocketIO(app, logger=True, engineio_logger=False)
+
+#
+# HTML Pages
+#
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/cam')
+def cam():
+    return render_template('cam.html')
+
+@app.route('/avi')
+def avi():
+    return render_template('avi.html')
+
+
+#
+# From Cam/Screen to UI connections
+#
+
+@sio.on('connect', namespace='/ui')
+def connect_ui():
+    print('ui client connected: {}'.format(request.sid))
+
+
+@sio.on('disconnect', namespace='/ui')
+def disconnect_ui():
+    print('ui client disconnected: {}'.format(request.sid))
+
+
+@sio.on('connect', namespace='/cam')
+def connect_cam():
+    print('[Cam client connected: {}'.format(request.sid))
+
+
+@sio.on('disconnect', namespace='/cam')
+def disconnect_cam():
+    print('Cam client disconnected: {}'.format(request.sid))
+
+
+@sio.on('cam2server', namespace='/cam')
+def handle_cam_message(message):
+    # print('Send msg to server2ui')
+    sio.emit('server2ui', message, namespace='/ui')
+
+
+#
+# Rest / Cloud Event to AVI page
+#
+
+@sio.on('connect', namespace='/ui2')
+def connect_ui2():
+    print('ui2 client connected: {}'.format(request.sid))
+
+@sio.on('disconnect', namespace='/ui2')
+def disconnect_ui2():
+    print('ui2 client disconnected: {}'.format(request.sid))
+
+
+
+@app.route('/', methods=['POST'])
+def process_event():
+    app.logger.debug(request.headers)
+
+    app.logger.debug(request.data.decode("utf-8"))
+    data = json.loads(request.data.decode("utf-8"))
+
+    sio.emit('server2ui2', data, namespace='/ui2')
+
+
+    response = make_response({})
+    return response
+
+if __name__ == '__main__':
+
+    app.logger.setLevel(logging.DEBUG)
+    sio.run(app=app, host='0.0.0.0', port=8080)
+
---
+++ dashboard/test-curl.sh
@@ -0,0 +1,13 @@
+curl -v "http://localhost:8080/" \
+-X POST \
+-H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f79" \
+-H "Ce-specversion: 1.3" \
+-H "Ce-Type: manuela.image-processor.response" \
+-H "Ce-Source: manuela/eventing/image-processor" \
+-H "Content-Type: application/json" \
+-d '{     
+            "image": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAIBAQEBAQIBAQECAgICAgQDAgICAgUEBAMEBgUGBgYFBgYGBwkIBgcJBwYGCAsICQoKCgoKBggLDAsKDAkKCgr/2wBDAQICAgICAgUDAwUKBwYHCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgr/wAARCAADAAMDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD8l/FnxU8VQ6uoe00KcvYWkhe78K6fMw3W8bbFaSAlY1ztSMYSNFVECoqqCiisIxjyrQ6VCFtj/9k=",   
+            "id": 0,
+            "status": 0,
+            "text": "status 0"              
+        }'
